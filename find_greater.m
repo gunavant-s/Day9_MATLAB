c0ef66f1ccc827c3f373a9025a315f79e59ff8ca
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2023-07-28 07:36:54 +0530

    Update find_greater.m

--- find_greater.m
+++ find_greater.m
@@ -1,8 +1,8 @@
-function max = find_greater(n)
-    max = n(1);
+function max_n = find_greater(n)
+    max_n = n(1);
     for i = 2:length(n)
-        if max < n(i)
-            max = n(i);
+        if max_n < n(i)
+            max_n = n(i);
         end
     end
 end
